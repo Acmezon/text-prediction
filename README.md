0e15a3b2dcfa714cd591c2c47dd1cb82f5c77c2d
dandelea <dandelea@alum.us.es>
2016-02-15 19:40:57 +0100

    Letter learning

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# text-prediction
-Trabajo para la asignatura IA
+# Acme-Supermarket
+Aplicación text-prediction para la asignatura AIA (Aplicaciones de Inteligencia Artificial) del Máster de Ingeniería Informática de la Universidad de Sevilla. Predicción de texto por teclado T9.
+
+### Desarrollado por 
+
+* Daniel de los Reyes Leal
+* Alejandro Sánchez Medina
--- main.py
+++ main.py
@@ -1,48 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from sys import argv;
-import string;
+import json, re, string, os.path;
+
+filename = 'corpus/corpus_sm.txt';
+letter_monograms = 'training/monogram_letter.json';
+letter_bigrams = 'training/bigram_letter.json'
 
 def main():
-	script, filename = argv;
-	txt = open(filename);
-	letters = list(string.ascii_lowercase);
-	dict = {};
-
-	mapping_letters = {1:["."],
-		2:["a", "b", "c"],
-		3:["d" "e", "f"],
-		4:["g", "h", "i"],
-		5:["j", "k", "l"],
-		6:["m", "n", "ñ", "o"],
-		7:["p", "q", "r", "s"],
-		8:["t", "u", "v"],
-		9:["w", "x", "y", "z"]};
-
-
+	train();
+	
 	query = input('Input word query: ');
 
+	out = "";
 	for word in query.split():
 		# Conversion to number
 		number_query = word_to_number(word);
 
 		new_word = "";
-		for i in range (0, number_query):
+		last_letter = None;
+		for i in range (0, len(number_query)):
 			number = int(number_query[i]);
 			# Obtain out letter for number
-			letter = unigram_by_letters(number)[0];
+			if i == 0:
+				# first character of word
+				letter = predict_letter(number);
+			else:
+				letter = predict_letters(last_letter, number);
+			last_letter = letter;
 			new_word += letter;
+		out += new_word + " "
+	print(out)
 
-		print (new_word)
 
+## Save letter ocurrences in JSON format
+def train():
+	txt = open(filename).read();
+	letters = list(string.ascii_lowercase);
+	ocurrences = {};
+	bigrams = {};
 
+	monogram_learned = os.path.exists(letter_monograms);
+	bigram_learned = os.path.exists(letter_bigrams);
+
+	if not monogram_learned or not bigram_learned:
+		for letter in letters:
+			if not monogram_learned:
+				ocurrences[letter] = txt.count(letter);
+
+			if not bigram_learned:
+				for letter2 in letters:
+					bigrams[letter + letter2] = txt.count(letter + letter2);
+
+	if not monogram_learned:
+		with open(letter_monograms, 'w') as outfile:
+			json.dump(ocurrences, outfile);
+
+	if not bigram_learned:
+		with open(letter_bigrams, 'w') as outfile:
+			json.dump(bigrams, outfile);
 		
-## Returns sorted array of most probable letters for a dial.
-def unigram_by_letters(dial):
-	#TODO
+## Returns most probable letter for a dial number.
+def predict_letter(dial):
 
+	mapping_letters = {
+		2:["a", "b", "c"],
+		3:["d", "e", "f"],
+		4:["g", "h", "i"],
+		5:["j", "k", "l"],
+		6:["m", "n", "o"],
+		7:["p", "q", "r", "s"],
+		8:["t", "u", "v"],
+		9:["w", "x", "y", "z"]};
+
+	with open(letter_monograms) as data_file:
+		ocurrences = json.load(data_file);
+
+	candidates = mapping_letters[dial];
+
+	max = candidates[0];
+	for candidate in candidates:
+		if ocurrences[candidate] > ocurrences[max]:
+			max = candidate;
+	return max;
+
+## Returns most probable letter for a dial number with previous dial.
+def predict_letters(first_letter, second_number):
+	mapping_letters = {
+		2:["a", "b", "c"],
+		3:["d", "e", "f"],
+		4:["g", "h", "i"],
+		5:["j", "k", "l"],
+		6:["m", "n", "o"],
+		7:["p", "q", "r", "s"],
+		8:["t", "u", "v"],
+		9:["w", "x", "y", "z"]};
+
+	with open(letter_bigrams) as data_file:
+		ocurrences = json.load(data_file);
+
+	candidates = mapping_letters[second_number];
+
+	max = candidates[0];
+	for candidate in candidates:
+		if ocurrences[first_letter+candidate] > ocurrences[first_letter + max]:
+			max = candidate;
+	return max;
 
 ## Returns a dial code string for a word.
 ## Omits special characters
-def word_to_number(word): 
+def word_to_number(word):
+	word.replace('.,?!#@$%^&*_~-£()[]{}|', '');
+	word.replace('á', 'a')
+	word.replace('é', 'e')
+	word.replace('í', 'i')
+	word.replace('ó', 'o')
+	word.replace('ú', 'u')
 	number = "";
 	mapping_number = {
 		".": 1,
@@ -60,7 +134,6 @@ def word_to_number(word):
 		"l": 5,
 		"m": 6,
 		"n": 6,
-		"ñ": 6,
 		"o": 6,
 		"p": 7,
 		"q": 7,
@@ -74,28 +147,16 @@ def word_to_number(word):
 		"y": 9,
 		"z": 9};
 
+
 	for i in range (0, len(word)):
 		character = word[i];
-
-		if (character in '?!#@$%^&*_~-£()[]{}|'):
-			word.replace(character, '');
-
-		if (mapping_number[character]):
+		
+		if (character in mapping_number.keys()):
 			number += str(mapping_number[character]);
 		else:
 			print('Character ' + character + ' not recognised');
 
 	return number;
-
-
-
-
-	
-	
-
-
-
-
 
 
 if __name__=='__main__':
